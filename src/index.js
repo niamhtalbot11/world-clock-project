f71d6e4fc15fbdcef673363daa14924cba937e1b
niamhtalbot11 <74904383+niamhtalbot11@users.noreply.github.com>
2023-06-21 11:14:40 +0200

    selected cities

--- src/index.js
+++ src/index.js
@@ -34,6 +34,25 @@ dublinDateElement.innerHTML = dublinTimezone.format("dddd, MMMM D, YYYY");
 dublinTimeElement.innerHTML = dublinTimezone.format("h:mm:ss");
 }
 
+ function showCity(event){
+    let cityTimeZone= event.target.value;
+    let cityName = cityTimeZone.replace("_"," ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+        citiesElement.innerHTML = `
+        <div class="city row">${cityName}</div>
+        <div class="row">
+        <div class="date col">${cityTime.format("MMMM D, YYYY")}</div>
+        <div class="time col">${cityTime.format("h:mm:ss")}</div>
+        </div>
+        `;
+ }
+ 
+
+let citiesSelectElement = document.querySelector("#selectCity");
+citiesSelectElement.addEventListener("change", showCity);
+
+
 setInterval(updateLosAngelesTime, 1);
 setInterval(updateSydneyTime, 1);
 setInterval(updateDublinTime,1);
