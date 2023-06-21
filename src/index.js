eb62005b59ad9c199487c02fe01076c75b16b432
niamhtalbot11 <74904383+niamhtalbot11@users.noreply.github.com>
2023-06-21 11:44:05 +0200

    added current location

--- src/index.js
+++ src/index.js
@@ -36,6 +36,9 @@ dublinTimeElement.innerHTML = dublinTimezone.format("h:mm:ss");
 
  function showCity(event){
     let cityTimeZone= event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
+      }
     let cityName = cityTimeZone.replace("_"," ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
